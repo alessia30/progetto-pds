28bfdde262bcdac5ac0cfb7fa63413f7eb1a5e20
VanessaScherma <vanessa.scherma@gmail.com>
2023-10-28 23:29:28 +0200

    Shortcut button updated

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,17 @@ use eframe::egui;
 use screenshots::Screen;
 use std::time::Instant;
 
+use egui::Key;
+
+// Struttura per gestire la chiave e la corrispettiva stringa
+struct KeyData {
+    key: Key,
+    key_string: String,
+}
+
 fn main() -> Result<(), eframe::Error> {
+
+
     let options = eframe::NativeOptions {
         initial_window_size: Some(egui::vec2(400.0, 240.0)),
         ..Default::default()
@@ -13,8 +23,18 @@ fn main() -> Result<(), eframe::Error> {
     let ritardi = vec!["Nessun ritardo", "3 secondi", "5 secondi","10 secondi"];
     let mut cattura=0;
     let mut ritardo=0;
+    
+    // Variabili per rappresentare la chiave e la stringa
+    let mut key_data = KeyData {
+        key: Key::S,
+        key_string: "S".to_string(),
+    };
+
+    let mut is_shortcut_modal_open = false;
+
     eframe::run_simple_native("My egui App", options, move |ctx, _frame| {
         egui_extras::install_image_loaders(ctx);
+
         egui::TopBottomPanel::top("my_panel").show(ctx, |ui| {
             ui.horizontal(|ui| {
                 if ui.button("\u{2795} Nuovo").on_hover_text("Nuova Cattura").clicked() {
@@ -48,14 +68,30 @@ fn main() -> Result<(), eframe::Error> {
                 }).response.on_hover_text("Ritarda cattura");
                 ui.add_space(20.0);
                 
-                
-                if ui.add(egui::ImageButton::new(egui::include_image!("../img/add.png"))).on_hover_text("Nuova Cattura").clicked() {
-                    
+                // Bottone "Modifica Shortcut"
+                if ui.add(egui::ImageButton::new(egui::include_image!("../img/settings.png"))).on_hover_text("Modifica shortcut").clicked() {
+                    is_shortcut_modal_open = true;
                 }
             });
-        });
-        egui::CentralPanel::default().show(ctx, |ui| {
 
+            // Gestione della window per la modifica della shortcut
+            if is_shortcut_modal_open {
+                egui::Window::new("Finestra Modale").show(&ctx, |ui| {
+                    ui.label("Questo è il contenuto della finestra modale.");
+            
+                    if ui.button("Chiudi").clicked() {
+                        is_shortcut_modal_open = false; // Chiudi la finestra modale quando l'utente fa clic su "Chiudi"
+                    }
+                });
+            }
+            
+        });
+
+        // --- PANNELLO CENTRALE ---
+        egui::CentralPanel::default().show(ctx, |ui| {
+            ui.centered_and_justified(|ui| {
+                ui.heading(&format!("Premi il tasto {} per catturare il contenuto dello schermo senza avviare l'Applicazione", key_data.key_string));
+             });
         });
     })
 }
